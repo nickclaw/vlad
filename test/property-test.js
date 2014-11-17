1236ff0962f7f0d81cef832df4e563fbe187dcdc
Nicholas Clawson <nickclaw@gmail.com>
2014-11-16 19:01:29 -0800

    Add tests for property behavior. Part of issue #3

---
+++ test/property-test.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect,
+    property = require('../src/property');
+
+describe('property definition', function() {
+
+    it('can be extended', function() {
+        var newProperty = property.extend();
+        expect(newProperty).to.not.equal(property);
+        expect(property).to.equal(newProperty.__proto__);
+        expect(property).to.deep.equal(newProperty);
+    });
+
+    it('should chain', function() {
+        var prop = property.extend();
+        expect(prop.default().required.strict).to.equal(prop);
+    });
+
+    it('correctly parses schema', function() {
+        var schema = property.extend().required.strict.toSchema();
+        expect(schema).to.deep.equal({
+            required: true,
+            strict: true
+        });
+    });
+});
