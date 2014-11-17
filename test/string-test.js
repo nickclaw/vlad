1536dda0de5803d7fb7516ba370f751316e263b2
Nicholas Clawson <nickclaw@gmail.com>
2014-11-16 19:35:25 -0800

    Add string property test. Issue #3

---
+++ test/string-test.js
@@ -0,0 +1,65 @@
+describe('string property validation', function() {
+
+    describe('maxLength', function() {
+
+        var validate = vlad({
+            val: vlad.string.maxLength(9)
+        });
+
+        it('throws when the string is too long', function() {
+            return validate({val: '0123456789'}).should.be.rejected;
+        });
+
+        it('doesnt throw otherwise', function() {
+            return validate({val: ''}).should.be.fulfilled;
+        });
+    });
+
+    describe('minLength', function() {
+
+        var validate = vlad({
+            val: vlad.string.minLength(5)
+        });
+
+        it('throws when the string is too short', function() {
+            return validate({val: ''}).should.be.rejected;
+        });
+
+        it('doesnt throw otherwise', function() {
+            return validate({val: '12345'}).should.be.fulfilled;
+        });
+    });
+
+    describe('pattern', function() {
+
+        var validate = vlad({
+            val: vlad.string.pattern(/^[A-Z]+$/)
+        });
+
+        it('throws when the pattern doesnt match', function() {
+            return validate({val: 'ABCabc'}).should.be.rejected;
+        });
+
+        it('doesnt throw otherwise', function() {
+            return validate({val: 'ABCABC'}).should.be.fulfilled;
+        });
+    });
+
+    describe('within', function() {
+
+        var validate = vlad({
+            val: vlad.string.within(5, 9)
+        });
+
+        it('throws when the string is not within', function() {
+            return validate({val: 'a'}).should.be.rejected
+            .then(function() {
+                return validate({val: '0123456789'}).should.be.rejected
+            });
+        });
+
+        it('doesnt throw otherwise', function() {
+            return validate({val: '0123456'}).should.be.fulfilled;
+        });
+    });
+});
