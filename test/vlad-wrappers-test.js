55009a43d6dd5000950c4537f72a508f3f3eb2ef
Nicholas Clawson <nickclaw@gmail.com>
2015-03-13 17:53:12 -0700

    Updated vlad.middleware argument order, deprecated old order

--- test/vlad-wrappers-test.js
+++ test/vlad-wrappers-test.js
@@ -11,7 +11,7 @@ describe('callback style validation', function() {
             a: 'test',
             b: 1
         }, function(err, data) {
-            expect(err).to.equal(null);
+            expect(err).to.be.null;
             expect(data.a).to.equal('test');
             expect(data.b).to.equal(1);
             expect(data.c).to.equal(10);
@@ -35,11 +35,11 @@ describe('callback style validation', function() {
 
 describe('middleware style validation', function() {
 
-    var validate = vlad.middleware({
+    var validate = vlad.middleware('body', {
         a: vlad.string,
         b: vlad.number,
         c: vlad.integer.default(10)
-    }, 'body');
+    });
 
     it("should accept a valid value", function(done) {
         var res = {},
@@ -48,7 +48,7 @@ describe('middleware style validation', function() {
             };
 
         validate(req, res, function(err) {
-            expect(err).to.equal(null);
+            expect(err).to.be.undefined;
             expect(req.body.a).to.equal('test');
             expect(req.body.b).to.equal(1);
             expect(req.body.c).to.equal(10);
@@ -69,4 +69,75 @@ describe('middleware style validation', function() {
         });
     });
 
+
+
+    it('should default to query', function(done) {
+
+        var validate = vlad.middleware({
+            a: vlad.string.default('test')
+        });
+
+        var req = {query: {}},
+            res = {};
+
+        validate(req, res, function() {
+            expect(req.query.a).to.equal('test');
+            done();
+        });
+
+    });
 });
+
+//
+// Testing deprecated version
+// Remove by version 0.12.0
+//
+describe('deprecated middleware style validation', function() {
+    var oldValidate = vlad.middleware({
+        a: vlad.string,
+        b: vlad.number,
+        c: vlad.integer.default(10)
+    }, 'body');
+
+    it("should accept a valid value", function(done) {
+        var res = {},
+            req = {
+                body: {a: 'test', b: 1}
+            };
+
+        oldValidate(req, res, function(err) {
+            expect(err).to.be.undefined;
+            expect(req.body.a).to.equal('test');
+            expect(req.body.b).to.equal(1);
+            expect(req.body.c).to.equal(10);
+            done();
+        });
+    });
+    it("should reject an invalid value", function(done) {
+        var res = {},
+            req = {
+                body: {a: 1, b: 'test'}
+            };
+
+        oldValidate(req, res, function(err) {
+            expect(err).to.be.instanceof(vlad.GroupValidationError);
+            expect(err.fields.a).to.be.instanceof(vlad.FieldValidationError);
+            done();
+        });
+    });
+    it('should default to query', function(done) {
+
+        var validate = vlad.middleware({
+            a: vlad.string.default('test')
+        });
+
+        var req = {query: {}},
+            res = {};
+
+        validate(req, res, function() {
+            expect(req.query.a).to.equal('test');
+            done();
+        });
+
+    });
+})
